4f3764a530243c95ff36c8c3e51c8c53ab228323
Santiago Herrera <santiago.herrera@snapsheet.me>
2015-12-02 15:45:36 -0600

    creating branch from last issue if no number specified

--- github/branch_from_issue.py
+++ github/branch_from_issue.py
@@ -5,10 +5,14 @@ import sys
 import re
 
 api = GithubAPIGateway(token=os.environ['GITHUB_TOKEN'])
-issue_number = sys.argv[1]
-issue_title = api.call('list_issue', owner='bodyshopbidsdotcom', repo='snapsheet', number=issue_number)['title']
+issue = None
+if len(sys.argv) <= 1:
+  issue = api.call('list_issues', org='bodyshopbidsdotcom')[0]
+else:
+  issue = api.call('list_issue', owner='bodyshopbidsdotcom', repo='snapsheet', number=sys.argv[1])
 
-branch_name = issue_number + '-' + re.sub('\s+', '-', issue_title.strip())
+issue_title = issue['title']
+branch_name = str(issue['number']) + '-' + re.sub('[\s+\:]+', '-', issue_title.strip())
 
 git = Repo(os.getcwd()).git
 git.checkout('HEAD', b=branch_name)
