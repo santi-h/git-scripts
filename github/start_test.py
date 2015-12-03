f6b8104d15fb6e8fd7f23adae5b056e93572b768
Santiago Herrera <santiago.herrera@snapsheet.me>
2015-12-03 03:23:55 -0600

    displaying build url

--- github/start_test.py
+++ github/start_test.py
@@ -5,8 +5,8 @@ import os
 current_branch = Repo(os.getcwd()).active_branch
 api = CircleCiAPIGateway(token=os.environ['CIRCLE_TOKEN'])
 result = api.call('new_build', username='bodyshopbidsdotcom', project='snapsheet', branch=current_branch)
-if result.get('build_num') is not None:
-  print 'Build {0} started'.format(result['build_num'])
+if result.get('build_url') is not None:
+  print result['build_url']
 else:
   if result.get('message') is not None:
     print result['message']
