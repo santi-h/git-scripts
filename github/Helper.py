bef00c32134b30efd522d1dc65e681a1b847e763
Santiago Herrera <santiago.herrera@snapsheet.me>
2015-12-07 09:01:53 -0600

    adding owner and repo

--- github/Helper.py
+++ github/Helper.py
@@ -1,4 +1,4 @@
-from git import Repo
+import git
 import re
 import os
 
@@ -6,5 +6,17 @@ def branch_name(issue):
   return str(issue['number']) + '-' + re.sub('[^\w\d]+', '-', issue['title'].strip().lower())[0:30].strip('-')
 
 def create_branch(branch_name):
-  git = Repo(os.getcwd()).git
-  git.checkout('HEAD', b=branch_name)
+  g = git.Repo(os.getcwd()).git
+  g.checkout('HEAD', b=branch_name)
+
+def owner_and_repo():
+  g = git.cmd.Git(os.getcwd())
+  remotes = g.execute(['git','remote','-v'])
+  match = re.search('github\.com:([\w\-]+)\/([\w\-]+)\.git \(fetch\)', remotes)
+  owner = None
+  repo = None
+  if match is not None:
+    owner = match.group(1).encode('ascii')
+    repo = match.group(2).encode('ascii')
+
+  return owner, repo
