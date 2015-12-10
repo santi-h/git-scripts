6c722ea33d6070de82ddf5554b2d5c78f8407f6f
Santiago Herrera <santiago.herrera@snapsheet.me>
2015-12-09 21:47:49 -0600

    OAuth2.0 in WrikeAPIGateway

--- github/WrikeAPIGateway.py
+++ github/WrikeAPIGateway.py
@@ -1,7 +1,52 @@
 from APIGateway import APIGateway
+import os
+import json
+import webbrowser
+import SocketServer
+import socket
+import BaseHTTPServer
+import re
+import Helper
+import threading
+
+class WrikeCodeServer(BaseHTTPServer.BaseHTTPRequestHandler):
+  def __init__(self, *args):
+    self.code = None
+    BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, *args)
+
+  def do_GET(self):
+    match = re.search('code=([\w|\-]+)', self.path)
+    if match is not None:
+      self.server.authentication_code = match.group(1)
+      while self.server.redirect is None: pass
+      self.send_response(301)
+      self.send_header('Location', self.server.redirect)
+      self.end_headers()
+    else:
+      self.server.authentication_code = 0
+      self.send_response(406)
+      self.end_headers()
+
+class QuickSocketServer(SocketServer.TCPServer):
+  def __init__(self):
+    self.authentication_code = None
+    self.redirect = None
+    SocketServer.TCPServer.__init__(self, ("", 19877), WrikeCodeServer)
+
+  def server_bind(self):
+    self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.socket.bind(self.server_address)
+
+class ServerThread(threading.Thread):
+  def __init__(self, httpd, **args):
+    self._httpd = httpd
+    threading.Thread.__init__(self, **args)
+
+  def run(self):
+    self._httpd.handle_request()
 
 class WrikeAPIGateway(APIGateway):
-  def __init__(self, **args):
+  def __init__(self, redirect=None):
     self._host_url = 'https://www.wrike.com/api/v3'
     self._api = {
       'refresh_token': {
@@ -18,11 +63,18 @@ class WrikeAPIGateway(APIGateway):
         'method': 'POST',
         'params': {
           'grant_type': 'authorization_code'
-        }
+        },
+        'valid_status': [200]
       },
       'get_task': {
         'path': '/tasks/{id}',
-        'method': 'GET'
+        'method': 'GET',
+        'translate': [
+          {
+            'type': 'ApiV2Task',
+            'params': ['id']
+          }
+        ]
       },
       'id': {
         'path': '/ids',
@@ -31,12 +83,100 @@ class WrikeAPIGateway(APIGateway):
     }
 
     self._common_headers = {}
-    if args.get('token') is not None:
-      self._common_headers['Authorization'] = 'bearer {0}'.format(args['token'])
-
     self._common_params = {}
+    self._auth_info = {}
+    self._redirect = redirect
+    self._httpd = None
+    self._serverthread = None
 
   def update_common_headers(self, data):
     self._common_headers = {
       'Authorization': 'bearer {0}'.format(data['access_token'])
     }
+
+  def redirect(self, redirect="http://www.google.com"):
+    if redirect is not None:
+      if self._httpd is not None:
+        self._httpd.redirect = redirect
+        self._httpd = None
+
+      if self._serverthread is not None:
+        self._serverthread.join()
+        self._serverthread = None
+
+  def call(self, api, **args):
+    self._authenticate_client()
+    return self._call(api, **args)
+
+  def _call(self, api, **args):
+    if self._api[api].get('translate') is not None:
+      for translate in self._api[api]['translate']:
+        for param in translate['params']:
+          args[param] = self._call('id', params={
+            'ids': '[{0}]'.format(args[param]),
+            'type': translate['type']
+          })[0]['data'][0]['id']
+
+    result, status = super(WrikeAPIGateway, self).call(api, **args)
+
+    if status == 401 and result['error'] == 'not_authorized':
+      self._refresh_client_authentication()
+      result, status = super(WrikeAPIGateway, self).call(api, **args)
+
+    return result, status
+
+  def _refresh_client_authentication(self):
+    auth_info = super(WrikeAPIGateway, self).call('refresh_token', params={
+      'client_id': os.environ['WRIKE_CLIENT_ID'],
+      'client_secret': os.environ['WRIKE_CLIENT_SECRET'],
+      'refresh_token': self._auth_info['refresh_token']
+    })[0]
+
+    with open(_get_auth_file_filepath(), 'w') as outfile:
+      json.dump(auth_info, outfile)
+
+    self._auth_info = auth_info
+    api.update_common_headers(auth_info)
+
+  def _authenticate_client(self):
+    auth_info = self._get_auth_info()
+    if auth_info is None:
+      auth_info = self._create_auth_info()
+
+    self._auth_info = auth_info
+    self.update_common_headers(auth_info)
+
+  def _create_auth_info(self):
+    webbrowser.open('https://www.wrike.com/oauth2/authorize?client_id={0}&response_type=code'.format(os.environ['WRIKE_CLIENT_ID']))
+    self._httpd = QuickSocketServer()
+    self._serverthread = ServerThread(self._httpd)
+    self._serverthread.start()
+    while self._httpd.authentication_code is None: pass
+    authentication_code = self._httpd.authentication_code
+    if self._redirect is not None:
+      self.redirect(self._redirect)
+
+    owner, repo = Helper.owner_and_repo()
+    auth_info = super(WrikeAPIGateway, self).call('get_token', params={
+      'client_id': os.environ['WRIKE_CLIENT_ID'],
+      'client_secret': os.environ['WRIKE_CLIENT_SECRET'],
+      'code': authentication_code
+    })[0]
+    with open(self._get_auth_file_filepath(), 'w') as outfile:
+      json.dump(auth_info, outfile)
+
+    return auth_info
+
+  def _get_auth_info(self):
+    filepath = self._get_auth_file_filepath()
+    data = None
+    if os.path.isfile(filepath):
+      with open(filepath) as data_file:
+        data = json.load(data_file)
+
+    return data
+
+  def _get_auth_file_filepath(self):
+    data_file_dirpath = os.path.dirname(os.path.realpath(__file__))
+    data_file_filename = os.path.splitext(os.path.basename(__file__))[0] + '.json'
+    return data_file_dirpath + '/' + data_file_filename
--- github/issue_from_wrike.py
+++ github/issue_from_wrike.py
@@ -2,13 +2,7 @@ from WrikeAPIGateway import WrikeAPIGateway
 from GithubAPIGateway import GithubAPIGateway
 from git import Repo
 import os
-import sys
 import re
-import SocketServer
-import BaseHTTPServer
-import socket
-import json
-import webbrowser
 import Helper
 import argparse
 
@@ -16,56 +10,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--nobranch", action="store_true")
 parser.add_argument("taskid")
 args = parser.parse_args()
-
-class QuickSocketServer(SocketServer.TCPServer):
-  def server_bind(self):
-    self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    self.socket.bind(self.server_address)
-
-class WrikeCodeServer(BaseHTTPServer.BaseHTTPRequestHandler):
-  def do_GET(self):
-    code = re.search('code=([\w|\-]+)', self.path).group(1)
-    data = create_data_file(code)
-    issue_url = do_it(data)
-    self.send_response(301)
-    self.send_header('Location', issue_url)
-    self.end_headers()
-
-def get_data_file_filepath():
-  data_file_dirpath = os.path.dirname(os.path.realpath(__file__))
-  data_file_filename = os.path.splitext(os.path.basename(__file__))[0] + '.json'
-  return data_file_dirpath + '/' + data_file_filename
-
-# returns a json with the info in the data file or None if there's non data file
-def get_data_file():
-  filepath = get_data_file_filepath()
-  data = None
-  if os.path.isfile(filepath):
-    with open(filepath) as data_file:
-      data = json.load(data_file)
-
-  return data
-
-# makes the api call to wrike to get the authentication code and returns it, or None if it failed
-def get_authentication_code_and_do_it():
-  webbrowser.open('https://www.wrike.com/oauth2/authorize?client_id={0}&response_type=code'.format(os.environ['WRIKE_CLIENT_ID']))
-  httpd = QuickSocketServer(("", 19877), WrikeCodeServer)
-  httpd.handle_request()
-
-# creates the data info file by using the authentication_code and requesting a token
-# returns the json info
-def create_data_file(authentication_code):
-  api = WrikeAPIGateway()
-  owner, repo = Helper.owner_and_repo()
-  data = api.call('get_token', owner=owner, repo=repo, params={
-    'client_id': os.environ['WRIKE_CLIENT_ID'],
-    'client_secret': os.environ['WRIKE_CLIENT_SECRET'],
-    'code': authentication_code
-  })[0]
-  with open(get_data_file_filepath(), 'w') as outfile:
-    json.dump(data, outfile)
-
-  return data
 
 def create_issue(task):
   api = GithubAPIGateway(token=os.environ['GITHUB_TOKEN'])
@@ -85,40 +29,10 @@ def create_branch(issue):
   git.checkout('HEAD', b=branch_name)
   return branch_name
 
-def api_wrapper(api, auth_data, method, **args):
-  result, status = api.call(method, **args)
-  if status == 401 and result['error'] == 'not_authorized':
-    data = api.call('refresh_token', params={
-      'client_id': os.environ['WRIKE_CLIENT_ID'],
-      'client_secret': os.environ['WRIKE_CLIENT_SECRET'],
-      'refresh_token': auth_data['refresh_token']
-    })[0]
-
-    with open(get_data_file_filepath(), 'w') as outfile:
-      json.dump(data, outfile)
-
-    api.update_common_headers(data)
-    result = api.call(method, **args)[0]
-
-  return result
-
-def do_it(auth_data):
-  api = WrikeAPIGateway(token=auth_data['access_token'])
-  result = api_wrapper(api, auth_data, 'id', params= {
-    'ids': '[{0}]'.format(args.taskid),
-    'type': 'ApiV2Task'
-  })
-  idv3 = result['data'][0]['id']
-  task = api_wrapper(api, auth_data, 'get_task', id=idv3)['data'][0]
-  issue = create_issue(task)
-  if args.nobranch == False:
-    create_branch(issue)
-  print issue['html_url']
-  return issue['html_url']
-
-data = get_data_file();
-if data is None:
-  get_authentication_code_and_do_it()
-else:
-  do_it(data)
-
+api = WrikeAPIGateway()
+task = api.call('get_task', id=args.taskid)[0]['data'][0]
+issue = create_issue(task)
+if args.nobranch == False:
+  create_branch(issue)
+print issue['html_url']
+api.redirect(issue['html_url'])
